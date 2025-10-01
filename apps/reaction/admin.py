0c25c818edd671936f6265914d2bd9171da1df0a
myra2004 <amirovamuqaddas@gmail.com>
2025-10-01 15:04:28 +0500

    edit: admin uv models

--- apps/reaction/admin.py
+++ apps/reaction/admin.py
@@ -5,41 +5,41 @@ from .models import Comment, Searched, View, Like, Rate, Review, Bookmark
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ("id", "item__title",)
-    list_display_links = ("id")
+    list_display_links = ("id",)
 
 
 @admin.register(Searched)
 class SearchedAdmin(admin.ModelAdmin):
         list_display = ("id", "item__title",)
-        list_display_links = ("id")
+        list_display_links = ("id",)
 
 
 @admin.register(View)
 class ViewAdmin(admin.ModelAdmin):
     list_display = ("id", "item__title",)
-    list_display_links = ("id")
+    list_display_links = ("id",)
 
 
 @admin.register(Like)
 class LikeAdmin(admin.ModelAdmin):
     list_display = ("id", "item__title",)
-    list_display_links = ("id")
+    list_display_links = ("id",)
 
 
 @admin.register(Rate)
 class RateAdmin(admin.ModelAdmin):
     list_display = ("id", "item__title", "rating")
-    list_display_links = ("id")
+    list_display_links = ("id",)
     search_fields = ("item__title", "rating")
 
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ("id", "item__title",)
-    list_display_links = ("id")
+    list_display_links = ("id",)
 
 
 @admin.register(Bookmark)
 class BookmarkAdmin(admin.ModelAdmin):
     list_display = ("id", "item__title",)
-    list_display_links = ("id")
+    list_display_links = ("id",)
