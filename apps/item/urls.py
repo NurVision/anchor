eab94c235e21e30c7e64264720527307daf39fdf
jumayevjavohir585 <star.boy.javohir2004@gmail.com>
2025-10-02 17:29:11 +0500

    feat: category views

--- apps/item/urls.py
+++ apps/item/urls.py
@@ -14,7 +14,17 @@ from apps.item.views import (
 app_name = "apps.item"
 
 urlpatterns = [
+<<<<<<< HEAD
     
+=======
+    # Item uchun yo'llar (sizning branch'ingizdan)
+    path("item/list/", ItemListAPIView.as_view(), name="item-list"),
+    path("item/detail/<int:id>/", ItemDetailAPIView.as_view(), name="item-detail"),
+    path("item/post/", ItemCreateAPIView.as_view(), name="item-create"),
+    path("item/update/<int:id>/", ItemUpdateAPIView.as_view(), name="item-update"),
+    path("item/delete/<int:id>/", ItemDeleteAPIView.as_view(), name="item-delete"),
+
+>>>>>>> ed08a4c (feat: category views)
     # Category uchun yo'llar (master branch'idan)
     path('categories/', views.CategoryListView.as_view(), name='category-list'),
     path('categories/<int:pk>/', views.CategoryDetailView.as_view(), name='category-detail'),
