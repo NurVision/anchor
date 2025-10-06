72f94c7e99b801970b3fa30af9591bbfa193573b
jumayevjavohir585 <star.boy.javohir2004@gmail.com>
2025-10-06 15:57:25 +0500

    fix: admin

--- apps/users/views/profile.py
+++ apps/users/views/profile.py
@@ -1,4 +1,4 @@
-from rest_framework import permissions, status
+from rest_framework import permissions, status, parsers
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
