52e6b2ee377d8cfb530ad57d08d8d062aede9a4d
myra2004 <amirovamuqaddas@gmail.com>
2025-10-06 11:19:42 +0500

    fix: pagination

--- core/settings/base.py
+++ core/settings/base.py
@@ -95,7 +95,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.AllowAny",
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'core.pagination.LimitPagePagination',
     "PAGE_SIZE": 10,
 
 }
